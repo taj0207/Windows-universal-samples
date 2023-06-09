a7e6fa1d1dd45c20ae14d5b0c81e8b8869d6e0e9
AJ Tsai <antsai@microsoft.com>
2023-06-08 22:40:00 -0700

    updated

--- Samples/SmartCard/cs/Scenario7_ListAllCards.xaml.cs
+++ Samples/SmartCard/cs/Scenario7_ListAllCards.xaml.cs
@@ -118,19 +118,16 @@ namespace SDKTemplate
         }
         async void cardadded(SmartCardReader reader, CardAddedEventArgs args)
         {
-            Guid g;
             foreach(SmartCardListItem scli in cardItems)
             {
                 if (scli.Reader == reader)
                 {
                     SmartCardProvisioning provisioning = await SmartCardProvisioning.FromSmartCardAsync(args.SmartCard);
-                    g = (await provisioning.GetIdAsync());
-                    scli.CardNames.Add(g.ToString());
-                    
+                    scli.CardNames.Add(await provisioning.GetNameAsync());
                     break;
                 }
             }
-            rootPage.NotifyUser("Add card to card reader: " + reader.Name + " " + g.ToString(), NotifyType.StatusMessage);
+            rootPage.NotifyUser("Add card to card reader: " + reader.Name , NotifyType.StatusMessage);
         }
         async void cardremoved(SmartCardReader reader, CardRemovedEventArgs args)
         {
@@ -143,8 +140,7 @@ namespace SDKTemplate
                     break;
                 }
             }
-            rootPage.NotifyUser("Remove card to card reader: " + reader.Name, NotifyType.StatusMessage);
-            
+            rootPage.NotifyUser("Remove card from card reader: " + reader.Name, NotifyType.StatusMessage);
         }
     }
 }
